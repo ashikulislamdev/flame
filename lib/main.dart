cff48aae229d05c8eae1e5bfc2529da66c49825d
saun2648 <ashikulislamsawan@gmail.com>
2022-05-19 01:21:01 +0600

    load sprite compunent and set position by x and y cordinate

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,20 @@ void main() {
 class MyGame extends FlameGame{
   @override
   Future<void> onLoad() async{
-    print("We will import our asset here");
+    SpriteComponent girl = SpriteComponent();
+    SpriteComponent boy = SpriteComponent();
+    super.onLoad();
+    girl
+      ..sprite = await loadSprite("girl.png")
+      ..size = Vector2(200, 200)
+      ..y = 100
+      ..x = 100;
+    add(girl);
+
+    boy
+      ..sprite = await loadSprite('boy.png')
+      ..size = Vector2(200, 200)
+      ..y = 300;
+    add(boy);
   }
 }
