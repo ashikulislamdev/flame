f18844035710d16eede1ac14509a8c759263c7d9
saun2648 <ashikulislamsawan@gmail.com>
2022-05-19 13:26:17 +0600

    get screen size

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,29 @@ class MyGame extends FlameGame{
   Future<void> onLoad() async{
     SpriteComponent girl = SpriteComponent();
     SpriteComponent boy = SpriteComponent();
+    SpriteComponent bgImg = SpriteComponent();
+
+    const double charSize = 200.00;
     super.onLoad();
+    final screenWidth = size[0];
+    final screenHeight = size[1];
+    const textBoxHeight = 96;
+
+    add(bgImg 
+    ..sprite = await loadSprite('game-bg.jpg')
+    ..size = size);
+
     girl
       ..sprite = await loadSprite("girl.png")
-      ..size = Vector2(200, 200)
-      ..y = 100
-      ..x = 100;
+      ..size = Vector2(charSize, charSize)
+      ..y = screenHeight - charSize - textBoxHeight;
     add(girl);
 
     boy
       ..sprite = await loadSprite('boy.png')
-      ..size = Vector2(200, 200)
-      ..y = 300;
+      ..size = Vector2(charSize, charSize)
+      ..y = screenHeight - charSize - textBoxHeight
+      ..x = screenWidth - charSize;
     add(boy);
   }
 }
